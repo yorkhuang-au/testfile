2103227b08dd814467a38bbfe00dd1af7bebc4a4
York Huang <york.y.huang@gmail.com>
2015-07-01 06:07:43 -0700

    update

--- src/DollarInputFormat.java
+++ src/DollarInputFormat.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 /**
  * 
@@ -18,10 +19,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 public class DollarInputFormat extends FileInputFormat<LongWritable, Text> {
 
 	@Override
-	public RecordReader<LongWritable, Text> createRecordReader(InputSplit arg0,
+	public RecordReader<LongWritable, Text> createRecordReader(InputSplit split,
 			TaskAttemptContext arg1) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return new DollarLineRecordReader((FileSplit) split);
 	}
 
 }
